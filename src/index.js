c13463a91a04cd11192ec7cbd2f64e77e5b07f25
matildasmeds <matilda.smeds@gmail.com>
2017-09-13 07:54:56 +0200

    Tutorial completed...

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ var m = require("mithril");
 
 var UserList = require("./views/UserList");
 var UserForm = require("./views/UserForm");
- 
-m.route(document.body, "/list", 
-	{
-	    "/list": UserList,
-	    "/edit/:id": UserForm,
+var Layout = require("./views/Layout");
+
+m.route(document.body, "/list", {
+    "/list": {
+	render: function() {
+	    return m(Layout, m(UserList));
 	}
-);
+    },
+    "/edit/:id": {
+	render: function(vnode) {
+	    return m(Layout, m(UserForm, vnode.attrs));
+	}
+    }
+});
