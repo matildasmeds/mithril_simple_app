8c1a8468439c6817abb9b391b4131eefb6bf86b3
matildasmeds <matilda.smeds@gmail.com>
2017-10-06 07:37:46 +0200

    WIP add pagination elements

--- src/views/Pagination.js
+++ src/views/Pagination.js
@@ -5,25 +5,39 @@ var Pagination = {
     total: null,
     limit: null,
     offset: null,
-    pageCount: null,
+    pageCount: 0,
+    items: [],
     update: function(opts) {
 	Pagination.total = opts.total;
 	Pagination.limit = opts.limit;
 	Pagination.offset = opts.offset;
+	var pageCountWas = Pagination.pageCount;
 	Pagination.pageCount = Math.floor( opts.total / opts.limit );
 	if (opts.total % opts.limit !== 0) {
 	    Pagination.pageCount += 1;
 	}
+	if (pageCountWas !== Pagination.pageCount) {
+	    Pagination.refreshItems();
+	}
+    },
+    refreshItems: function() {
+	var items = [];
+	for (var i=0; i < Pagination.pageCount; i++) {
+	    items.push(m("li.pagination-item", i + 1));
+	}
+	Pagination.items = items;
     },
     view: function() {
 	return m(".pagination-region",
 		 [m("p", "Pagination WIP"),
-		  m("ul.temp",
-		    [m("li", "total: " + Pagination.total),
-		     m("li", "pageCount: " + Pagination.pageCount),
-		     m("li", "limit: " + Pagination.limit),
-		     m("li", "offset: " + Pagination.offset)]
-		   )
+		  m("ul.temp", [
+		      m("li", "total: " + Pagination.total),
+		      m("li", "pageCount: " + Pagination.pageCount),
+		      m("li", "limit: " + Pagination.limit),
+		      m("li", "offset: " + Pagination.offset)
+		  ]),
+		  m("ul.pagination-list", Pagination.items
+		  ),
 		 ]
 		);
     }
